94face7e6cc4c2e58704b3655e061f9b2752ffae
Anthony Nahas <anthony.na@hotmail.de>
2017-03-16 08:59:58 +0100

    FEAT: @Target Android version starting with API 23: Requesting Permissions!

--- src/android/de/dff/cordova/plugins/IpAddress.java
+++ src/android/de/dff/cordova/plugins/IpAddress.java
@@ -1,5 +1,14 @@
 package de.dff.cordova.plugins;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
@@ -7,73 +16,102 @@ import java.net.NetworkInterface;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 /**
  * IP Address plugin
+ *
  * @author dff
  */
 public class IpAddress extends CordovaPlugin {
- 
-	@Override
+
+    private static final String TAG = "IpAddress";
+    private static final int PERMISSION_CODE = 0;
+    private static final String[] PERMISSIONS =
+        {
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.READ_PHONE_STATE
+        };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!cordova.hasPermission(PERMISSIONS[0])) {
+            getPermissions(PERMISSION_CODE);
+        } else if (!cordova.hasPermission(PERMISSIONS[1])) {
+            getPermissions(PERMISSION_CODE);
+        } else if (!cordova.hasPermission(PERMISSIONS[2])) {
+            getPermissions(PERMISSION_CODE);
+        }
+    }
+
+    private void getPermissions(int requestCode) {
+        cordova.requestPermissions(this, requestCode, PERMISSIONS);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                Log.e(TAG, "PERMISSIONS DENIED");
+                return;
+            }
+        }
+    }
+
+    @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    	try {
-			callbackContext.success(getIPAddress());
-            
+        try {
+            callbackContext.success(getIPAddress());
+
             return true;
-		}
-		catch (Exception ex) {
-			callbackContext.error(ex.getMessage());
-            
+        } catch (Exception ex) {
+            callbackContext.error(ex.getMessage());
+
             return false;
-		}
-        
+        }
+
     }
 
     /**
      * Get All Networkinterfaces from device
-     * @return  JSONArray
+     *
+     * @return JSONArray
      */
     public static JSONArray getIPAddress() throws Exception {
-		JSONArray jsonInterfaces = new JSONArray();
-		JSONArray jsonAddresses;
-		JSONObject jsonAddress;
-		List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-		for (NetworkInterface intf : interfaces) {
-			JSONObject jsonInterface = new JSONObject();
-			
-			jsonInterface.put("displayName", intf.getDisplayName());
-			jsonInterface.put("hardwareAddress", intf.getHardwareAddress());
-			jsonInterface.put("name", intf.getName());
-			
-			jsonAddresses = new JSONArray();			
-			List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
-			
-			for (InetAddress addr : addrs) {
-				if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
-					jsonAddress = new JSONObject();
-					jsonAddress.put("hostName", addr.getHostAddress().toUpperCase());
-					jsonAddress.put("isIPv4", true);
-					jsonAddresses.put(jsonAddress);
-				} 
-				else if (!addr.isLoopbackAddress() && addr instanceof Inet6Address){
-					jsonAddress = new JSONObject();
-					String sAddr = addr.getHostAddress().toUpperCase();
-					int delim = sAddr.indexOf('%');
-					jsonAddress.put("hostName", delim < 0 ? sAddr : sAddr.substring(0, delim));
-					jsonAddress.put("isIPv4", false);
-					jsonAddresses.put(jsonAddress);
-				}
-			}
-				
-			jsonInterface.put("addresses", jsonAddresses);
-			jsonInterfaces.put(jsonInterface);
-		}
-		return jsonInterfaces;
-	}
+        JSONArray jsonInterfaces = new JSONArray();
+        JSONArray jsonAddresses;
+        JSONObject jsonAddress;
+        List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+        for (NetworkInterface intf : interfaces) {
+            JSONObject jsonInterface = new JSONObject();
+
+            jsonInterface.put("displayName", intf.getDisplayName());
+            jsonInterface.put("hardwareAddress", intf.getHardwareAddress());
+            jsonInterface.put("name", intf.getName());
+
+            jsonAddresses = new JSONArray();
+            List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+
+            for (InetAddress addr : addrs) {
+                if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
+                    jsonAddress = new JSONObject();
+                    jsonAddress.put("hostName", addr.getHostAddress().toUpperCase());
+                    jsonAddress.put("isIPv4", true);
+                    jsonAddresses.put(jsonAddress);
+                } else if (!addr.isLoopbackAddress() && addr instanceof Inet6Address) {
+                    jsonAddress = new JSONObject();
+                    String sAddr = addr.getHostAddress().toUpperCase();
+                    int delim = sAddr.indexOf('%');
+                    jsonAddress.put("hostName", delim < 0 ? sAddr : sAddr.substring(0, delim));
+                    jsonAddress.put("isIPv4", false);
+                    jsonAddresses.put(jsonAddress);
+                }
+            }
+
+            jsonInterface.put("addresses", jsonAddresses);
+            jsonInterfaces.put(jsonInterface);
+        }
+        return jsonInterfaces;
+    }
 }
 
